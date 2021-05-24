9a53a91d03dc510d6f1457e9383f38fb3fe0d1ff
Nadiyahr <84344050+Nadiyahr@users.noreply.github.com>
2021-05-24 19:31:26 +0200

    Update homework1.js

--- homework1.js
+++ homework1.js
@@ -34,7 +34,47 @@ let isAdult = confirm('Are you of age?')
 
 console.log(isAdult);
 
+
+//-------Task 6-------//
+
+let firstName, lastName, studieGroup, year;
+
+firstName = 'Nadia'; //string
+lastName = 'Hrabcenko'; //string
+studieGroup = 602; //number
+year = 1979; //number
+
+let isMaried = true; //boolean
+
+console.log(studieGroup, year, isMaried, firstName, lastName);
+
+let isNull = null;
+let isUndef;
+
+console.log(typeof isNull, typeof isUndef);
+
+//-------Task 7-------//
+
+let user = prompt('Enter your usernamee here.', '');
+let mail = prompt('Enter your email address here.', '');
+let pass = prompt('Enter your password here.', '');
+
+document.write(`Dear ${user}, your email is ${mail}, your password is ${pass}.<br>`);
+
+//-------Task 8-------//
+
+let hour = 3600;
+let day = 24 * hour;
+let month = 30 * day;
+
+document.write(`One hour has ${hour} seconds.<br>One day has ${day} seconds.<br>One month has ${month} seconds.`);
+
+
+
+
 //------HOMEWORK 2------//
+
+
 
 //------Task 1-------//
 
@@ -203,37 +243,3 @@ else if (hour >= 17 && hour < 23) {
 else {
   document.write('Доброї ночі.');
 }
-
-//-------Task 6-------//
-
-let firstName, lastName, studieGroup, year;
-
-firstName = 'Nadia'; //string
-lastName = 'Hrabcenko'; //string
-studieGroup = 602; //number
-year = 1979; //number
-
-let isMaried = true; //boolean
-
-console.log(studieGroup, year, isMaried, firstName, lastName);
-
-let isNull = null;
-let isUndef;
-
-console.log(typeof isNull, typeof isUndef);
-
-//-------Task 7-------//
-
-let user = prompt('Enter your usernamee here.', '');
-let mail = prompt('Enter your email address here.', '');
-let pass = prompt('Enter your password here.', '');
-
-document.write(`Dear ${user}, your email is ${mail}, your password is ${pass}.<br>`);
-
-//-------Task 8-------//
-
-let hour = 3600;
-let day = 24 * hour;
-let month = 30 * day;
-
-document.write(`One hour has ${hour} seconds.<br>One day has ${day} seconds.<br>One month has ${month} seconds.`);
