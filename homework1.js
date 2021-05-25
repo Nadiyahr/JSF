9fb50a5b8bf2d5718b2a9349d84764e12ac1dbd8
Nadiya Hr <nadiahrb79@gmail.com>
2021-05-25 11:15:41 +0200

    homevork3

--- homework1.js
+++ homework1.js
@@ -270,6 +270,7 @@ let count = 0;
 
 while (count < array.length) {
   m_while *= array[count];
+  count++;
 }
 
 console.log('While result ' + m_while);
@@ -294,3 +295,68 @@ document.write(check);
 function randArray(k) {
 
 }
+
+
+//------Task 3------//
+
+
+function randArray(k) {
+  let arr = [];
+  for (let i = 0; i < k; i++) {
+    arr.push( Math.floor( Math.random() * 501 ))
+  }
+  return arr;
+}
+
+console.log(randArray(5));
+
+
+//------Task 4------//
+
+
+function raiseToDegree(a, b) {
+  return a ** b;
+}
+
+
+//------Task 5------//
+
+
+function findMind() {
+  return Math.min(...arguments);
+}
+
+console.log(findMind(4, 6, 1, -5, 7, 9));
+
+
+//------Task 6------//
+
+
+function findUnique(arr) {
+  const setArr = new Set(arr);
+  return setArr.size === arr.length;
+}
+
+console.log(findUnique([1, 9, 2, 9, 3, 5]));
+
+
+//------Task 7------//
+
+
+function lastElem(arr, num = 1) {
+  return arr.slice(-num)
+}
+
+  console.log(lastElem([3, 4, 10, -5], 2));
+
+
+  //------Test 8------//
+
+
+  function transformFirst(str) {
+    let first = str.slice(0, 1).toUpperCase();
+    let rest = str.slice(1)
+    return first + rest;
+  }
+
+  console.log(transformFirst('i love java script') )
