755e9931cf3c4617c13d054690a2836d6cb9bda4
Nadiyahr <84344050+Nadiyahr@users.noreply.github.com>
2021-06-20 13:27:51 +0300

    Add files via upload

---
+++ homework9.js
@@ -0,0 +1,32 @@
+//------Homework 9------//
+
+//------Task 1------//
+
+$('a[href^="https://"]').attr('target', '_blank');
+
+
+//------Task 2------//
+
+
+$('h2.head').css('background', 'green').find('.inner').css('fontSize', '35px');
+
+
+//------Task 3------//
+
+
+$('h3').each(function(){
+  $(this).next().insertBefore($(this))
+});
+
+
+
+//------Task 4------//
+
+
+
+  $('input[type=checkbox]').on('change', function() {
+    if ($('input[type=checkbox]:checked').length >= 4) {
+      $(this).prop('checked', false);
+      $('#check').text('Allowed only 3')
+    }
+  })
